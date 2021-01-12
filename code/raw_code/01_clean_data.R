0ba72496746b3cd60a64d1e26df88f4485ab2634
morganjacobus <morganjacobus@gmail.com>
2021-01-11 21:52:28 -0500

    Update Clean Data Script
    Adding code to clean up survey responses raw data including removing identical records, reformatting dates, andremoving unecessary columns,

--- code/raw_code/01_clean_data.R
+++ code/raw_code/01_clean_data.R
@@ -1,49 +1,32 @@
 
-  ## Import Bus Stop Census Survey Responses
-  raw_df <- fromJSON(here("data", "raw_data", "survey", "2020-12-28 stopcensus_responses.json"))
+  # Import Bus Stop Census Survey Responses
+  raw_df <- read.csv(here("data", "raw_data", "survey", "2021-01-01_raw_bus_stop_census_responses.csv"))
 
-  ## Clean field names for ease of use in analysis
-  survey_df <- raw_df %>% rename(Stop_ID = stopid,
-                                 Crosswalk_Features = `Crosswalk Features`,
-                                 Line_of_Sight = `Line of Sight Blockage`,
-                                 Direction = `Stop Direction`,
-                                 On_Site_Survey = `On-site`,
-                                 Trash_Can = `Trash Can`,
-                                 Main_Street = `On Street`,
-                                 Behavior = `Dangerous Behavior`,
-                                 Nearest_Landmark = `Nearest Landmark`,
-                                 Routes = `Serving Routes`,
-                                 Email = From,
-                                 Lighting = `Stop Lighting`,
-                                 Boarding_Area = `Boarding Area`,
-                                 Wayfinding = Information,
-                                 Wayfinding_Accessibility = `Poster Height`,
-                                 Obstacle_Desc = `Obstacle Description`,
-                                 Additional_Comments = `Additional Comments`) %>%
-                          select(Stop_ID, Timestamp,Email, Main_Street, Nearest_Landmark,
-                                 Routes, Direction, Seating, Shelter, Trash_Can,
-                                 Cleanliness, Line_of_Sight, Wayfinding, Wayfinding_Accessibility,
-                                 Lighting, Sidewalk, Obstacles, Obstacle_Desc,
-                                 Boarding_Area, Crosswalks, Crosswalk_Features,
-                                 Behavior, On_Site_Survey, Additional_Comments) %>%
-                              distinct()
+  # Clean data of identical records
+  raw_df_deduped <- raw_df %>% distinct_at(vars(-Record_ID), .keep_all = TRUE)
   
-  ## Clean Timestamp
-  survey_df$Timestamp_Sub <- substring(survey_df$Timestamp, 0, 24) 
-  survey_df$Timestamp_Sub <- as.POSIXct(survey_df$Timestamp_Sub, format = "%a %b %d %Y %H:%M:%S")
+  # Merge 'Another.HUGE.inconvenience' into 'Additional_Comments' field and remove
+  raw_df_deduped_clean <- raw_df_deduped %>% mutate(Additional_Comments = 
+                                                        ifelse(is.na(Another.HUGE.inconvenience), Additional_Comments,
+                                                               paste(Additional_Comments, Another.HUGE.inconvenience, sep = " "))) %>%
+                                             select(-Another.HUGE.inconvenience)
   
-  ## Move all emails to lower case for summarization
-  survey_df$Email <- tolower(survey_df$Email)
+  # Remove trailing columns
+  raw_df_deduped_clean <- raw_df_deduped_clean %>% select(-To, -Date, -Subject, -Cc, -X.From)
   
+  # Convert Timestamp To R Readable Format
+  raw_df_deduped_clean$Timestamp_Sub <- substring(raw_df_deduped_clean$Timestamp, 0, 24) # Take substring of timestamp to reformat
+  raw_df_deduped_clean$Timestamp_Sub <- as.POSIXct(raw_df_deduped_clean$Timestamp_Sub, format = "%a %b %d %Y %H:%M:%S") # Convert to POSIXct
+  raw_df_deduped_clean <- raw_df_deduped_clean %>% mutate(Timestamp = Timestamp_Sub) %>%
+                                                   select(-Timestamp_Sub)
   
-  #Specify subset of bus stops to analyze out of all bus stops
-  col_names <- c("Stop_ID", "Stop_Name", "Routes", "Surveyed")
-  subset <- read.csv("NPU-Z Bus IDs.csv", col.names = col_names)
-  subset$Stop_ID <- as.character(subset$Stop_ID)
-
-  subset_survey_df <- subset %>% inner_join(survey_df, by = c("Stop_ID"))
-                          #mutate(Surveyed_Stop = ifelse(is.na(Stop_ID_Join_Key), "N", "Y")) %>%
-                          #select(Stop_ID, Stop_Name, Routes, Surveyed_Stop)
+  # Collect GTFS Stop Data to Compare to Survey Responses
+  stop_comparison_data <- stops %>% select(stop_id, stop_name, stop_lat, stop_lon) %>%
+                                    separate(stop_name, c("main_street_or_station", "cross_street"), sep = "@")
+  
+  # Join Survey Responses and GTFS Data
+  raw_df_deduped_clean_joined <- raw_df_deduped_clean %>% left_join(stop_comparison_data, by = c("stop_id"))
+  
   
   #Data Output
   write.csv(survey_df, file = here("data", "tidy_data", "pre_processed_survey_df.csv"))
