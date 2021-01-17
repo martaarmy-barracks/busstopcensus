c6656538728ae865046d5d692ea7881108c1d23b
morganjacobus <morganjacobus@gmail.com>
2021-01-16 19:57:56 -0500

    Stop ID Clean Up Code and Duplicate Processing
    This commit includes code for filling in missing Stop IDs, filtering out unidentified stops IDs, and cleaning up the Stop ID attribute data. It also includes lines of code for pulling out the duplicates so the MARTA Army team can examine and modify according to our duplicate record processing protocol.

--- code/raw_code/01_clean_data.R
+++ code/raw_code/01_clean_data.R
@@ -25,21 +25,43 @@
                                              # mutate(name_diff = ifelse(stop_name.x != stop_name.y, "FLAG", ""),
                                              #         lat_diff = ifelse(stop_lat.x != stop_lat.y, "FLAG", ""),
                                              #         lon_diff = ifelse(stop_lon.x != stop_lon.y, "FLAG", "")) %>%
-                                              mutate(stop_code = ifelse(is.na(stop_code.x), stop_code.y, stop_code.x),
-                                                     stop_name = ifelse(is.na(stop_name.x), stop_name.y, stop_name.x),
+                                              mutate(stop_name = ifelse(is.na(stop_name.x), stop_name.y, stop_name.x),
                                                      stop_lat = ifelse(is.na(stop_lat.x), stop_lat.y, stop_lat.x),
                                                      stop_lon = ifelse(is.na(stop_lon.x), stop_lon.y, stop_lon.x)) %>%
-                                              select(stop_id, stop_code, stop_name, stop_lat, stop_lon) %>%
+                                              select(stop_id, stop_name, stop_lat, stop_lon) %>%
                                     separate(stop_name, c("main_street_or_station", "cross_street"), sep = "@")
   
-  raw_df_deduped_clean_joined <- raw_df_deduped_clean %>% left_join(stop_comparison_data, # Join Survey Responses and GTFS Data
+  no_stop_id_full_join <- raw_df_deduped_clean %>% left_join(stop_comparison_data, # Join Survey Responses and GTFS Data
                                                                     by = c("Stop_ID" = "stop_id")) 
-  
-  no_stop_id_df <- raw_df_deduped_clean_joined %>% dplyr::filter(is.na(main_street_or_station)) # Get list of Stop_IDs not in GTFS Data
-  
+  no_stop_id_df <- no_stop_id_full_join %>% dplyr::filter(is.na(main_street_or_station)) # Get list of Stop_IDs not in GTFS Data
   write.csv(no_stop_id_df, here("data", "raw_data", "validation", "No Stop ID Survey Records.csv"), row.names = FALSE) # Export records for examination
   
+  # Add Stop IDs to records with missing Stop IDs
+  'These Stop_IDs were manually identified using the data exported to the "No Stop ID Survey Records.csv" file and the Bus Stop Census map. Any records where the 
+  Stop ID could not be identified are filtered out of the dataset. The results of the manul identification are contained in the "Bus Stop Census Corrections Log" 
+  spreadhseet. In total, there were 17 responses with no identified Stop ID. Stop IDs were identified for five of these records, leaving 12 records without
+  Stop IDs remaining. These records are removed from the response data because they could not be linked to a bus stop.'
+  raw_df_deduped_clean <- no_stop_id_full_join %>% mutate(Stop_ID = case_when(Record_ID == 42 ~ "212715",
+                                                                              Record_ID == 1000 ~ "900948",
+                                                                              Record_ID == 3032 ~ "903549",
+                                                                              Record_ID == 3720 ~ "901906",
+                                                                              Record_ID == 7345 ~ "120024",
+                                                                              TRUE              ~ Stop_ID)) %>%
+                                                   dplyr::filter(!is.na(main_street_or_station)) #Remove remaining records from response data without a Stop ID.
+  
+  
   # Collect Duplicate Records by Stop_ID
+  stop_id_duplicates <- raw_df_deduped_clean %>% select(Stop_ID) %>%
+                                                   group_by(Stop_ID) %>%
+                                                   summarize(count = n()) %>%
+                                                   dplyr::filter(count > 1) %>%
+                                                   ungroup() %>%
+                                                   select(Stop_ID)
+  
+  # Create data.frame of only Stop_ID duplicates
+  duplicates_by_stop_id_df <- raw_df_deduped_clean %>% inner_join(stop_id_duplicates, by = c("Stop_ID")) %>%
+                                                       
+  
   
   #Data Output
   write.csv(survey_df, file = here("data", "tidy_data", "pre_processed_survey_df.csv"))
